5aa6478d2fc089d7cc12a247fd1ad6dbac9d8fc3
Oksana Kushchik <okushchik@gmail.com>
2020-02-08 13:48:11 +0200

    2

--- script.js
+++ script.js
@@ -5,30 +5,20 @@ let delBlock = document.querySelector('.delBlock');
 let editBlock = document.querySelector('.editBlock');
 let doneUl = document.querySelector('.doneUl');
 let filter = document.querySelector('#filter');
+let li;
+let task;
+let buttonNav;
+let butDone;
+let butEdit;
+let butDel;
 
-
-
-inputTask.onclick = function () {
-    inputTask.value = '';
-
-};
-
-
-buttonAdd.onclick = function () {
-    if(inputTask.value !== ''){
-        createElement();
-    }
-
-};
-function createElement() {
-
-    let li = document.createElement('li');
-    let task = document.createElement('div');
-    let buttonNav = document.createElement('div');
-    let butDone = document.createElement('button');
-    let butEdit = document.createElement('button');
-    let butDel = document.createElement('button');
-
+function createBlock () {
+    li = document.createElement('li');
+    task = document.createElement('div');
+    buttonNav = document.createElement('div');
+    butDone = document.createElement('button');
+    butEdit = document.createElement('button');
+    butDel = document.createElement('button');
 
     li.className = 'li';
     task.className = 'task';
@@ -48,6 +38,39 @@ function createElement() {
     butEdit.innerHTML = '<i class="fas fa-pen"></i>';
     butDel.innerHTML = '<i class="fas fa-trash"></i>';
 
+}
+/////////////////////
+let arr = [];
+
+if(localStorage.getItem('items')!== null) {
+    arr = JSON.parse(localStorage.getItem('items'))
+}
+
+for (let i = 0; i<arr.length; i++) {
+    createBlock ();
+    task.textContent = arr[i];
+    delItem (li,butDel);
+    editItem (butEdit,task);
+    doneItem (butDone,li);
+}
+//////////////////////////////////////////////
+inputTask.onclick = function () {
+    inputTask.value = '';
+};
+
+buttonAdd.onclick = function () {
+    if(inputTask.value !== ''){
+        createElement();
+
+
+    }
+};
+
+
+function createElement() {
+
+    createBlock();
+
     task.textContent = inputTask.value;
 
     delItem (li,butDel);
@@ -62,8 +85,20 @@ function createElement() {
     filter.onkeyup = function () {
         filters(task);
     };
+
+
+
+    arr.push(task.textContent);
+localStorage.setItem('items', JSON.stringify(arr));
+
 }
 
+console.log(document.querySelectorAll('li'));
+
+
+
+
+////////////////////////////////////////////////////////////
 function delItem(li,butDel) {
     butDel.addEventListener('click', function () {
         let yes = document.querySelector('.yes');
@@ -88,8 +123,15 @@ function editItem(butEdit,task) {
         editBlock.style.display = 'block';
         inputEditTask.value = this.parentElement.parentElement.innerText;
         save.onclick = function(){
-            task.innerHTML = inputEditTask.value;
+
+            task.textContent = inputEditTask.value;
+            localStorage.setItem('items', JSON.stringify(arr));
             editBlock.style.display = 'none';
+            arr = [];
+            for (let i = 0; i<document.querySelectorAll('li').length; i++) {
+                arr.push(document.querySelectorAll('li')[i].innerText);
+            }
+            localStorage.setItem('items', JSON.stringify(arr));
         };
         cancel.onclick = function(){
             editBlock.style.display = 'none';
@@ -108,9 +150,6 @@ function doneItem(butDone,li) {
         }
     });
 }
-
-
-
 
 function filters(task) {
     let text = filter.value.toLocaleLowerCase();
